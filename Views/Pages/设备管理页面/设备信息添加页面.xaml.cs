efdf42c78c903038875238623492a03b9527e7f1
litai <litai@DESKTOP-O137E18>
2024-09-05 16:32:00 +0800

    添加设备信息管理页面

---
+++ Views/Pages/设备管理页面/设备信息添加页面.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InventoryManagementSystem.Views.Pages.设备管理页面
+{
+    /// <summary>
+    /// 设备信息添加页面.xaml 的交互逻辑
+    /// </summary>
+    public partial class 设备信息添加页面 : Page
+    {
+        public 设备信息添加页面()
+        {
+            InitializeComponent();
+        }
+    }
+}
